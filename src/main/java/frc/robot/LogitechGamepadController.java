2144c3ac8574d205cb5d2473c537b8b85a4651c6
cabobalot <casey8cockrell@gmail.com>
2019-01-26 14:54:40 -0700

    first commit

---
+++ src/main/java/frc/robot/LogitechGamepadController.java
@@ -0,0 +1,91 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * Describes a Logitech Gamepad Controller.  This type of controller is used
+ * for driver control, and it simplifies and abstracts the raw joystick.
+ */
+public class LogitechGamepadController extends Joystick {
+    
+    //Buttons
+    public final int A_BUTTON = 1;
+    public final int B_BUTTON = 2;
+    public final int X_BUTTON = 3;
+    public final int Y_BUTTON = 4;
+    public final int LEFT_BUMPER = 5;
+    public final int RIGHT_BUMPER = 6;
+    public final int START_BUTTON = 8;
+    public final int LEFT_STICK_IN = 9;
+    
+    //Axes
+    public final int LEFT_X_AXIS = 0;
+    public final int LEFT_Y_AXIS = 1;
+    public final int LEFT_TRIGGER = 2;
+    public final int RIGHT_X_AXIS = 4;
+    public final int RIGHT_Y_AXIS = 5;
+    public final int RIGHT_TRIGGER = 3;
+    
+    public LogitechGamepadController(int pos) {
+        super(pos);
+    }
+    
+    public boolean getAButton() {
+        return super.getRawButton(A_BUTTON);
+    }
+    
+    public boolean getBButton() {
+        return super.getRawButton(B_BUTTON);
+    }
+    
+    public boolean getXButton() {
+        return super.getRawButton(X_BUTTON);
+    }
+    
+    public boolean getYButton() {
+        return super.getRawButton(Y_BUTTON);
+    }
+    
+    public boolean getLeftBumper() {
+        return super.getRawButton(LEFT_BUMPER);
+    }
+    
+    public boolean getRightBumper() {
+        return super.getRawButton(RIGHT_BUMPER);
+    }
+    
+    public boolean getStart() {
+        return super.getRawButton(START_BUTTON);
+    }
+    
+    public double getLeftX() {
+        return super.getRawAxis(LEFT_X_AXIS);
+    }
+    
+    public double getLeftY() {
+        return super.getRawAxis(LEFT_Y_AXIS);
+    }
+    
+    public double getLeftTrigger() {
+        return super.getRawAxis(LEFT_TRIGGER);
+    }
+    
+    public double getRightX() {
+        return super.getRawAxis(RIGHT_X_AXIS);
+    }
+    
+    public double getRightY() {
+        return super.getRawAxis(RIGHT_Y_AXIS);
+    }
+    
+    public double getRightTrigger() {
+        return super.getRawAxis(RIGHT_TRIGGER);
+    }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,176 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.GamepadBase;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+
+  SpeedControllerGroup leftTrain = new SpeedControllerGroup(new Spark(0), new Spark(1));
+  SpeedControllerGroup rightTrain = new SpeedControllerGroup(new Spark(2), new Spark(3));
+  private DifferentialDrive drivetrain;
+
+  private VictorSP intake = new VictorSP(4);
+  private VictorSP launcher = new VictorSP(5);
+
+  private Joystick stick;
+  private LogitechGamepadController gamepad;
+
+  private static final String kFlightStickDrive = "Flight Stick Drive";
+  private static final String kGamePadArcadeDrive = "Game Pad Arcade Drive";
+  private static final String kGamePadTankDrive = "Game Pad Tank Drive";
+  private static final String kGamePadStickDrive = "Game Pad Stick Drive";
+
+  private String m_DriveSelected;
+  private final SendableChooser<String> driveChooser = new SendableChooser<>();
+
+  private static final String kLeftPreference = "Left";
+  private static final String kRightPreference = "Right";
+
+  private String m_SidePreference;
+  private final SendableChooser<String> SideChooser = new SendableChooser<>();
+
+  private double rotateMultiplier = 0.6;
+  private double speedMultiplier = 0.85;
+
+
+  @Override
+  public void robotInit() {
+    drivetrain = new DifferentialDrive(leftTrain, rightTrain);
+    drivetrain.setDeadband(0.05);
+
+    stick = new Joystick(0);
+    gamepad = new LogitechGamepadController(1);
+
+    CameraServer.getInstance().startAutomaticCapture();
+
+    driveChooser.setDefaultOption(kFlightStickDrive, kFlightStickDrive);
+    driveChooser.addOption(kGamePadArcadeDrive, kGamePadArcadeDrive);
+    driveChooser.addOption(kGamePadTankDrive, kGamePadTankDrive);
+    driveChooser.addOption(kGamePadStickDrive, kGamePadStickDrive);
+    SmartDashboard.putData("Drive Choice", driveChooser);
+
+    SideChooser.addOption(kLeftPreference, kLeftPreference);
+    SideChooser.setDefaultOption(kRightPreference, kRightPreference);
+    SmartDashboard.putData("Side Choice", SideChooser);
+
+
+    SmartDashboard.putNumber("speed multiplier", speedMultiplier);
+    SmartDashboard.putNumber("rotate multiplier", rotateMultiplier);
+  }
+
+  @Override
+  public void robotPeriodic() {
+    rotateMultiplier = SmartDashboard.getNumber("rotate multiplier", 1);
+    speedMultiplier = SmartDashboard.getNumber("speed multiplier", 1);
+    SmartDashboard.putNumber("total", rotateMultiplier + speedMultiplier);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    m_DriveSelected = driveChooser.getSelected();
+    m_SidePreference = SideChooser.getSelected();
+
+    if (m_DriveSelected == kFlightStickDrive){
+      if (stick.getRawButton(2)){
+        intake.set(-0.9);
+      } else {
+        intake.set(0.0);
+      }
+
+      if (stick.getRawButton(1)){
+        launcher.set(-0.85);
+      } else if(stick.getRawButton(11)){
+        launcher.set(0.8);
+      } else {
+        launcher.set(0.0);
+      }
+    } else {
+      if (gamepad.getLeftBumper()){
+        intake.set(-0.9);
+      } else {
+        intake.set(0.0);
+      }
+
+      if (gamepad.getRightBumper()){
+        launcher.set(-0.85);
+      } else if(gamepad.getXButton()){
+        launcher.set(0.8);
+      } else {
+        launcher.set(0.0);
+      }
+
+    }
+
+    //drive
+    switch (m_DriveSelected) {
+      case kFlightStickDrive:
+        if (m_SidePreference == kLeftPreference) {
+          arcade(stick.getY(), stick.getZ());
+        } else if (m_SidePreference == kRightPreference){
+          arcade(stick.getY(), stick.getX());
+        }
+        break;
+
+      case kGamePadArcadeDrive:
+      if (m_SidePreference == kLeftPreference) {
+        arcade(gamepad.getLeftY(), gamepad.getRightX());
+      } else if (m_SidePreference == kRightPreference){
+        arcade(gamepad.getRightY(), gamepad.getLeftX());
+      }
+        break;
+
+      case kGamePadTankDrive:
+        tank(gamepad.getLeftY(), gamepad.getRightY());
+        break;
+
+      case kGamePadStickDrive:
+      if (m_SidePreference == kLeftPreference) {
+        arcade(gamepad.getLeftY(), gamepad.getLeftX());
+      } else if (m_SidePreference == kRightPreference){
+        arcade(gamepad.getRightY(), gamepad.getRightX());
+      }
+        break;
+
+      default:
+        drivetrain.stopMotor();
+        break;
+
+    }
+  }
+
+  private void arcade(double speed, double rotation) {
+    double outSpeed = - speedMultiplier * speed;
+
+    // double outRotation = rotation * speed * rotateMultiplier;
+    double outRotation = rotation * rotateMultiplier;
+
+    drivetrain.arcadeDrive(outSpeed, rotation, true);
+  }
+
+  private void tank(double left, double right) {
+    double outLeft = - left * speedMultiplier;
+    double outRight = - right * speedMultiplier;
+
+    drivetrain.tankDrive(outLeft, outRight, true);
+  }
+
+}
